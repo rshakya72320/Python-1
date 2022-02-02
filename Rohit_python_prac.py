77ee980c5b89c9405d709f47d77807099ab0ffcd
rshakya72320 <54726587+rshakya72320@users.noreply.github.com>
2022-02-02 08:12:51 -0600

    Add files via upload

---
+++ Rohit_python_prac.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+firstname = Rohit
+
+
+# In[2]:
+
+
+firstname = Rohit
+print(firstname)
+
+
+# In[3]:
+
+
+firstname = 'Rohit'
+print(firstname)
+
+
+# In[4]:
+
+
+name1 = 'shakya'
+print(name1)
+
+
+# In[7]:
+
+
+full_name = 'rohit shakya'
+print(full_name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+print(full_name.title())
+
+
+# In[8]:
+
+
+print(full_name.upper())
+
+
+# In[9]:
+
+
+print(full_name.lower())
+
+
+# In[10]:
+
+
+first_name = 'marshall'
+last_name = 'mathers'
+
+
+# In[11]:
+
+
+full_name = f" {first_name} {last_name}"
+print(full_name)
+
+
+# In[12]:
+
+
+message = f"keep up the good work , {full_name.title()}"
+print(message)
+
+
+# In[13]:
+
+
+print ('fav_prag_lang:pythonc++javacswiftcobalpascal')
+
+
+# In[14]:
+
+
+print('fav_prog_lang:\npython\nc++\njava\nrlanguage')
+
+
+# In[15]:
+
+
+name = ' siska'
+print(name.strip())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
