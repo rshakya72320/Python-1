281f29b3be3337305162fae5e4fd2cabf8a7c97c
rshakya72320 <54726587+rshakya72320@users.noreply.github.com>
2022-02-03 08:14:42 -0600

    Add files via upload

---
+++ Rohit_python_prac (1).py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+firstname = Rohit
+
+
+# In[2]:
+
+
+firstname = Rohit
+print(firstname)
+
+
+# In[3]:
+
+
+firstname = 'Rohit'
+print(firstname)
+
+
+# In[4]:
+
+
+name1 = 'shakya'
+print(name1)
+
+
+# In[7]:
+
+
+full_name = 'rohit shakya'
+print(full_name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+print(full_name.title())
+
+
+# In[8]:
+
+
+print(full_name.upper())
+
+
+# In[9]:
+
+
+print(full_name.lower())
+
+
+# In[10]:
+
+
+first_name = 'marshall'
+last_name = 'mathers'
+
+
+# In[11]:
+
+
+full_name = f" {first_name} {last_name}"
+print(full_name)
+
+
+# In[12]:
+
+
+message = f"keep up the good work , {full_name.title()}"
+print(message)
+
+
+# In[13]:
+
+
+print ('fav_prag_lang:pythonc++javacswiftcobalpascal')
+
+
+# In[14]:
+
+
+print('fav_prog_lang:\npython\nc++\njava\nrlanguage')
+
+
+# In[15]:
+
+
+name = ' siska'
+print(name.strip())
+
+
+# In[ ]:
+
+
+#Introduction to LIST DATABASE
+
+
+# In[1]:
+
+
+students = ['rohit', 'simran', 'naveen', 'kavya', 'sandeep', 'abdul']
+
+
+# In[2]:
+
+
+print(students)
+
+
+# In[3]:
+
+
+type(students)
+
+
+# In[4]:
+
+
+#Indexing
+
+
+# In[5]:
+
+
+print(student[4])
+
+
+# In[7]:
+
+
+print(students[4])
+
+
+# In[8]:
+
+
+print(students)
+
+
+# In[9]:
+
+
+students.append('hero')
+
+
+# In[10]:
+
+
+print(students)
+
+
+# In[11]:
+
+
+#adding Yash after Rohit.
+
+
+# In[13]:
+
+
+students.insert(1, 'Yash')
+
+
+# In[14]:
+
+
+print(students)
+
+
+# In[15]:
+
+
+#,modifying the name in the list.
+# lets say you want to modify the name of Yash to Phil then,
+
+
+# In[16]:
+
+
+print(students)
+
+
+# In[17]:
+
+
+students[1] = 'Phil'
+
+
+# In[18]:
+
+
+print(students)
+
+
+# In[19]:
+
+
+#deleting names
+
+
+# In[20]:
+
+
+del students[3]
+
+
+# In[21]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
